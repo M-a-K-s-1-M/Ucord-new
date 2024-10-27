536274344d1e8414e25f612355d82caecae2de4d
Arthur <serenkov03@mail.ru>
2024-10-27 18:38:16 +0500

    auth by email

--- ucord_service/src/main/java/com/example/ucord_service/security/UserDetailsServiceImpl.java
+++ ucord_service/src/main/java/com/example/ucord_service/security/UserDetailsServiceImpl.java
@@ -15,10 +15,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository repository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        User user = repository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found. Username is: " + username));
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
         return new AppUserDetails(user);
     }
