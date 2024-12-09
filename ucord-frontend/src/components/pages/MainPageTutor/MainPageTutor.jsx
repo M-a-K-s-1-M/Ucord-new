3d00c4780072c48c9f986dd76af98ef54fbe2d03
Емельянов Максим <masiko-17@mail.ru>
2024-12-10 01:50:15 +0500

    Создаст вёрстку раздела уведомлений у тьютора

--- ucord-frontend/src/components/pages/MainPageTutor/MainPageTutor.jsx
+++ ucord-frontend/src/components/pages/MainPageTutor/MainPageTutor.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react"
-import '../../Header/Header.css'
 import NotificationsSectionTutor from '../../NotificationsSection/NotificationsSectionTutor/NotificationsSectionTutor'
 import TicketsSectionTutor from "../../TicketsSectionTutor/TicketsSectionTutor";
-
+import './MainPageTutor.scss'
 
 export default function MainPageTutor() {
     const [active, setActive] = useState('notifications');
@@ -13,14 +12,15 @@ export default function MainPageTutor() {
                 <img className='main-logo' src='../../../public/logoMainPage.png' />
                 <nav>
                     <ul>
-                        {active === 'notifications' ? <li className='active'><a onClick={() => { setActive('notifications') }}>Уведомления</a></li> : <li><a onClick={() => { setActive('notifications') }}>Уведомления</a></li>}
-                        {active === 'tickets' ? <li className='active'><a onClick={() => { setActive('tickets') }}>Тикеты</a></li> : <li><a onClick={() => { setActive('tickets') }}>Тикеты</a></li>}
+                        {active === 'notifications' ? <li><a className='active' onClick={() => { setActive('notifications') }}>Уведомления</a></li> : <li><a onClick={() => { setActive('notifications') }}>Уведомления</a></li>}
+                        {active === 'tickets' ? <li><a className='active' onClick={() => { setActive('tickets') }}>Тикеты</a></li> : <li><a onClick={() => { setActive('tickets') }}>Тикеты</a></li>}
                     </ul>
                 </nav>
             </header>
 
 
-            {active === 'notifications' ? <NotificationsSectionTutor /> : <TicketsSectionTutor />}
+            {active === 'notifications' && <NotificationsSectionTutor />}
+            {active === 'tickets' && <TicketsSectionTutor />}
         </section>
     )
 }
